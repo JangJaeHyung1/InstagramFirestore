82adcea210a3169357620364f8a56564790a6dfb
JangJaeHyung1 <ggassoon@naver.com>
2021-05-08 23:27:50 +0900

    로그인 페이지 UI 구성

---
+++ InstagramFirestore/Controller/Authentication/LoginController.swift
@@ -0,0 +1,120 @@
+//
+//  LoginController.swift
+//  InstagramFirestore
+//
+//  Created by jh on 2021/05/06.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let iconImage: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.keyboardType = .emailAddress
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        tf.setHeight(50)
+        tf.attributedPlaceholder = NSAttributedString(string: "이메일", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.keyboardType = .emailAddress
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        tf.setHeight(50)
+        tf.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return tf
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("로그인", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "비밀번호를 잊어버리셨습니까?", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "", attributes: boldAtts))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        return button
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "계정이 없으시다면..    ", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "회원가입하기", attributes: boldAtts))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        return button
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemPink
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI(){
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
+        gradient.locations = [0,1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, forgotPasswordButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(signUpButton)
+        signUpButton.centerX(inView: view)
+        signUpButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+    }
+}
---
+++ InstagramFirestore/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterationController.swift
+//  InstagramFirestore
+//
+//  Created by jh on 2021/05/06.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemPink
+    }
+}
