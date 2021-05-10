07751dd8370818b34745afdcfd56a52737f0d6a8
JangJaeHyung1 <ggassoon@naver.com>
2021-05-10 21:37:34 +0900

    계정 생성 페이지 구성, 로그인 페이지와 중복되는 요소들을 구분하는 뷰모델을 프로토콜 방식으로 채택하도록함.

--- InstagramFirestore/ViewModel/AuthViewModel.swift
+++ InstagramFirestore/ViewModel/AuthViewModel.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
-struct LoginViewModel {
+protocol FormViewModel {
+    func updateForm()
+}
+
+protocol AuthViewModel {
+    var formIsValid: Bool { get }
+    var buttonBackgroundColor: UIColor { get }
+    var buttonTitleColor: UIColor { get }
+}
+
+struct LoginViewModel: AuthViewModel {
     var email: String?
     var password: String?
     
@@ -25,6 +35,22 @@ struct LoginViewModel {
     
 }
 
-struct RegistrationViewModel {
+struct RegistrationViewModel: AuthViewModel {
+
+    var email: String?
+    var password: String?
+    var fullName: String?
+    var userName: String?
     
+    var formIsValid: Bool {
+        return email?.isEmpty == false && password?.isEmpty == false && fullName?.isEmpty == false && userName?.isEmpty == false
+    }
+    
+    var buttonBackgroundColor: UIColor {
+        return formIsValid ? #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.3)
+    }
+    
+    var buttonTitleColor: UIColor {
+        return formIsValid ? .white : UIColor(white: 1, alpha: 0.6)
+    }
 }
