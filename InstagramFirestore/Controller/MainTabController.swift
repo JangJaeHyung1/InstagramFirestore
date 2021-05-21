7515f88f3b46cefb9f827f0b528c1dc50e24c04f
JangJaeHyung1 <ggassoon@naver.com>
2021-05-21 15:46:36 +0900

    YPImagePicker UI 적용, 갤러리허용

--- InstagramFirestore/Controller/MainTabController.swift
+++ InstagramFirestore/Controller/MainTabController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import YPImagePicker
 
 class MainTabController: UITabBarController {
     
@@ -51,6 +52,8 @@ class MainTabController: UITabBarController {
     func configureViewControllers(withUser user: User) {
         view.backgroundColor = .white
         
+        self.delegate = self
+        
         let layout = UICollectionViewFlowLayout()
         let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), seletedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
         
@@ -75,6 +78,15 @@ class MainTabController: UITabBarController {
         nav.navigationBar.tintColor = .black
         return nav
     }
+    
+    func didFinishPickMedia(_ picker: YPImagePicker) {
+        picker.didFinishPicking { items, _ in
+            picker.dismiss(animated: true) {
+                guard let selectedImage = items.singlePhoto?.image else { return }
+                print("DEBUG: selectedImage \(selectedImage)")
+            }
+        }
+    }
 }
 
 // MARK: - AuthenticationDelegate
@@ -84,5 +96,32 @@ extension MainTabController: AuthenticationDelegate {
         fetchUser()
 //        print("DEBUG: Auth did complete. Fetch user and update")
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - UITabBarControllerDelegate
+
+extension MainTabController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let index = viewControllers?.firstIndex(of: viewController)
+        
+        if index == 2 {
+            var config = YPImagePickerConfiguration()
+            config.library.mediaType = .photo
+            config.shouldSaveNewPicturesToAlbum = false
+            config.startOnScreen = .library
+            config.screens = [.library]
+            config.hidesStatusBar = false
+            config.hidesBottomBar = false
+            config.library.maxNumberOfItems = 1
+            
+            let picker = YPImagePicker(configuration: config)
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+            
+            didFinishPickMedia(picker)
+        }
+        
+        return true
     }
 }
--- InstagramFirestore/Controller/ProfileController.swift
+++ InstagramFirestore/Controller/ProfileController.swift
@@ -48,7 +48,7 @@ class ProfileController: UICollectionViewController {
             self.user.stats = stats
             self.collectionView.reloadData()
             
-            print("DEBUG: stats \(stats)")
+//            print("DEBUG: stats \(stats)")
         }
     }
     
