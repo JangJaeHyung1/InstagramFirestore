4b094eb191a9226b56ca21210f3194d9e0ebb32c
JangJaeHyung1 <ggassoon@naver.com>
2021-06-05 18:23:37 +0900

    notificationVC의 UI 구성

--- InstagramFirestore/Controller/NotificationsController.swift
+++ InstagramFirestore/Controller/NotificationsController.swift
@@ -7,10 +7,34 @@
 
 import UIKit
 
-class NotificationsController: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationsController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
         
-        view.backgroundColor = .systemOrange
+    }
+    
+    //MARK: - Helpers
+    
+    func configureTableView(){
+        view.backgroundColor = .white
+        navigationItem.title = "알림"
+        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+    }
+}
+
+extension NotificationsController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        
+        return cell
     }
 }
