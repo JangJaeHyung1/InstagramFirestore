66fb75415961aa238daf77bddb083f08a4cbb68c
JangJaeHyung1 <ggassoon@naver.com>
2021-06-07 21:00:12 +0900

    좋아요 눌렀을때 알림data를 firestore에 upload

---
+++ InstagramFirestore/Model/Notification.swift
@@ -0,0 +1,43 @@
+//
+//  Notification.swift
+//  InstagramFirestore
+//
+//  Created by jh on 2021/06/05.
+//
+
+import Firebase
+
+enum NotificationType: Int {
+    case like
+    case follow
+    case comment
+    
+    var notificationMessage: String {
+        switch self {
+        case .like:
+            return " 게시글을 좋아요 눌렀습니다."
+        case .follow:
+            return " 팔로우를 하였습니다."
+        case .comment:
+            return " 게시글에 댓글을 남겼습니다."
+        }
+    }
+}
+
+struct Notification {
+    let uid: String
+    var postImageUrl: String?
+    var postId: String?
+    let timestamp: Timestamp
+    let type: NotificationType
+    let id: String
+    
+    init(dictionary: [String: Any]) {
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.id = dictionary["id"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.postId = dictionary["postId"] as? String ?? ""
+        self.postImageUrl = dictionary["postImageUrl"] as? String ?? ""
+        self.type = NotificationType(rawValue: dictionary["type"] as? Int ?? 0) ?? .like
+    }
+}
